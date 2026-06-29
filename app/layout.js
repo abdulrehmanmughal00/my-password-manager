9a600a72b9c091d01fe955c2125da4ca0c23ac8d
abdul rehman <abdulrehmanmuglll00@gmail.com>
2026-06-29 11:05:25 +0500

    add metaData

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
+export const metadata = {
+  title: "My Password Manager",
+  description: "Securely store and manage your passwords.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -10,7 +15,6 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="PEczm1L0N6wB84I2zYd_-vR7Wpxd-ObZcE4yVfbdgMc"
         />
-
         <script src="https://cdn.lordicon.com/lordicon.js"></script>
       </head>
 
