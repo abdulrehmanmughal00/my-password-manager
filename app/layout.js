4511ac2da26d469fad37e2de6c0eae785bb69273
abdul rehman <abdulrehmanmuglll00@gmail.com>
2026-06-29 10:57:28 +0500

    add  meta tag

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="PEczm1L0N6wB84I2zYd_-vR7Wpxd-ObZcE4yVfbdgMc"
+        />
+
         <script src="https://cdn.lordicon.com/lordicon.js"></script>
       </head>
 
